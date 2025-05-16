e1d5dc3ba2394e01624c8a58e10086fcac2e30db
Ewi <ewiabeng@yahoo.com>
2025-05-16 11:00:30 +0000

    This is my first commit

---
+++ app.java
@@ -0,0 +1,9 @@
+echo "Please enter your name"
+            read name
+            echo "Please enter your password"
+            read -s password
+            echo "Welcome to Paypal application"echo "Please enter your name"
+            read name
+            echo "Please enter your password"
+            read -s password
+            echo "Welcome to Paypal application"
